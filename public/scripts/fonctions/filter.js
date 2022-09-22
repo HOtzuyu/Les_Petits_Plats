111fc3541f0c872a84d92e72fdf975a7f879d2fb
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2022-09-22 20:22:52 +0200

    algo2 v

--- public/scripts/fonctions/filter.js
+++ public/scripts/fonctions/filter.js
@@ -62,56 +62,6 @@ class Filter {
             });
         }
 
-    //     let open = false;
-    //     const input = this.DOMfilter.querySelector("input");
-    //     const placeholder = input.placeholder;
-    //     const filter = this.DOMfilter;
-    //     let othersFilters = Array.from(document.querySelectorAll(".filters__element"));
-    //     othersFilters = othersFilters.filter((elem) => {
-    //         return elem !== filter
-    //     });
-    //     const dropDownIcon = this.DOMfilter.querySelector("img");
-
-    //     this.DOMfilter.addEventListener("click", (e) => {
-    //         e.stopPropagation();
-    //         othersFilters.forEach(element => {
-    //             element.style.pointerEvents = "none";
-    //         });
-    //         // Open sort list
-    //         if (open == false) {
-    //             const placeholderMin = placeholder.toLowerCase();
-    //             filter.querySelector("ul").classList.remove("filters__element__list--hide");
-    //             input.classList.add("filters__element__button__input--after");
-    //             input.placeholder = `Rechercher un ${placeholderMin}`;
-    //             input.style.cursor = "text";
-    //             input.focus();
-    //             dropDownIcon.classList.add("dropdownIcon--after");
-    //             open = true;
-    //             document.addEventListener("click", function toggle(e) {
-    //                 if (!filter.contains(e.target)) {
-    //                     remove();
-    //                 }
-    //                 this.removeEventListener("click", toggle);
-    //             });
-    //         }
-    //         // Close sort list
-    //         else if (open == true && dropDownIcon.contains(e.target)) {
-    //             remove();
-    //         }
-    //     });
-
-    //     function remove() {
-    //         filter.querySelector("ul").classList.add("filters__element__list--hide");
-    //         input.classList.remove("filters__element__button__input--after");
-    //         input.placeholder = placeholder;
-    //         input.style.cursor = "pointer";
-    //         dropDownIcon.classList.remove("dropdownIcon--after");
-    //         othersFilters.forEach(element => {
-    //             element.style.pointerEvents = "unset";
-    //         });
-    //         open = false;
-    //     }
-    // }
 
 }
 
